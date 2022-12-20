d4a9be8fa5216029326533bcfd13cbd5e8707743
Iván Yamil Romero <31389316+knewlife@users.noreply.github.com>
2022-12-20 17:36:08 -0300

    animaciones con scroll

--- js/js.js
+++ js/js.js
@@ -65,4 +65,124 @@ let data = {
     }
 };
 
-console.log(data);
+//Helper functions
+//document.getElementById shortcut
+function did(el) {
+    return document.getElementById(el);
+}
+
+//document.getElementsByClassName shortcut
+function dc(el) {
+    return document.getElementsByClassName(el);
+}
+
+//console.log shortcut
+function l(txt) {
+    return console.log(txt)
+}
+
+//Animate CSS
+const animateCSS = (element, animation, prefix = 'animate__') =>
+    // We create a Promise and return it
+    new Promise((resolve, reject) => {
+        const animationName = `${prefix}${animation}`;
+        const node = element;
+
+        node.classList.add(`${prefix}animated`, animationName);
+
+        // When the animation ends, we clean the classes and resolve the Promise
+        function handleAnimationEnd(event) {
+            event.stopPropagation();
+            node.classList.remove(`${prefix}animated`, animationName);
+            resolve('Animation ended');
+        }
+
+        node.addEventListener('animationend', handleAnimationEnd, { once: true });
+    });
+
+
+//Functions and events
+//TODO: offsets
+function animateOnScroll(obj, refObj, animationIn, animationOut, duration, offsetIn, offsetOut) {
+
+    let currentPositionBottom = (document.documentElement.scrollTop + window.innerHeight);
+    let currentPositionTop = (document.documentElement.scrollTop);
+    let refObjRect = refObj.getBoundingClientRect();
+    let bodyRect = document.body.getBoundingClientRect();
+    let refPostionStart = refObjRect.top - bodyRect.top;
+    let refPostionEnd = refPostionStart + refObjRect.height;
+
+    //Set Animation duration
+    obj.style.setProperty('--animate-duration', duration);
+
+    //did('demo').style.top = refPostionStart + 'px';
+    //did('demo').style.height = (refObjRect.height) + 'px';
+    //console.clear();
+    // l('obj.Aniamted: ' + obj.animated);
+    // l('refPostionStart: ' + refPostionStart);
+    // l('refPostionStart+offsetIn: ' + (refPostionStart + offsetIn));
+    // l('refPostionEnd: ' + refPostionEnd);
+    // l('refPostionEnd+offsetOut: ' + (refPostionEnd + offsetOut));
+    // l('currentPositionBottom: ' + currentPositionBottom);
+    // l('currentPositionTop: ' + currentPositionTop);
+    // l('\n');
+
+    //TODO: DRYS
+    if (currentPositionTop > refPostionStart) {
+        if (!obj.animated)
+            return;
+    
+        l('animate Out Down');
+        animateCSS(obj, animationOut);
+        obj.addEventListener('animationend', () => {
+            obj.style.visibility = 'hidden';
+            obj.animated = false;
+            l(obj.style.visibility);
+        },{ once: true });
+    } else if (currentPositionBottom > refPostionStart) {
+        if (obj.animated)
+            return;
+
+        obj.animated = true;
+        l('animate In');
+        animateCSS(obj, animationIn);
+        obj.style.visibility = 'visible';
+        l(obj.style.visibility);
+    } else {
+        if (!obj.animated)
+            return;
+
+        l('animate Out Up');
+        animateCSS(obj, animationOut);
+        obj.addEventListener('animationend', () => {
+             obj.style.visibility = 'hidden';
+             obj.animated = false;
+             l(obj.style.visibility);
+        },{ once: true });
+    }
+
+}
+
+window.addEventListener('load', (e) => {
+    let domObj = {
+        "an1obj": did('an1'),
+        "s1obj": did('s1'),
+        "an2obj": did('an2'),
+        "s2obj": did('s2'),
+        "an3obj": did('an3'),
+        "s3obj": did('s3')
+    }
+
+    //Set animation state to false
+    domObj.an1obj.animated = false;
+    domObj.an2obj.animated = false;
+    domObj.an3obj.animated = false;
+
+
+
+    setInterval((e) => {
+        animateOnScroll(domObj.an1obj, domObj.s1obj, 'fadeInLeftBig', 'fadeOutLeftBig','0.5s', 100, 100);
+        animateOnScroll(domObj.an2obj, domObj.s2obj, 'fadeInLeftBig', 'fadeOutLeftBig','0.5s', 100, 100);
+        animateOnScroll(domObj.an3obj, domObj.s3obj, 'fadeInLeftBig', 'fadeOutLeftBig','0.5s', 100, 100);
+    }, 100);
+})
