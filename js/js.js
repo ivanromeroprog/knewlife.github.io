f3eac288b01f577b43c951a51f212f0b666d7ecb
Iván Yamil Romero <31389316+knewlife@users.noreply.github.com>
2022-12-22 13:10:28 -0300

    agregada carga de datos, arreglados detalles de diseño

--- js/js.js
+++ js/js.js
@@ -26,6 +26,26 @@ function l(txt) {
     return console.log(txt)
 }
 
+/**
+ * Escape special regex characters from string
+ * @param {string} str string to escape
+ * @returns 
+ */
+function escapeRegExp(str) {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
+
+/**
+ * Replace all ocurences of [find] in [str] with [replace]
+ * @param {string} str string in wich the search and replace will be performed
+ * @param {string} find text to find
+ * @param {string} replace text ro replace [find] ocurrences
+ * @returns 
+ */
+function replaceAll(str, find, replace) {
+    return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
+
 //Animate CSS
 /**
  * Trigger a animate.css animation by code.
@@ -67,8 +87,8 @@ const animateCSS = (element, animation, prefix = 'animate__') =>
  */
 function animateOnScroll(obj, refObj, animationIn, animationOut, duration, offsetIn, offsetOut) {
 
-    let currentPositionBottom = (document.documentElement.scrollTop + window.innerHeight)-offsetIn;
-    let currentPositionTop = (document.documentElement.scrollTop)+offsetOut;
+    let currentPositionBottom = (document.documentElement.scrollTop + window.innerHeight) - offsetIn;
+    let currentPositionTop = (document.documentElement.scrollTop) + offsetOut;
     let refObjRect = refObj.getBoundingClientRect();
     let bodyRect = document.body.getBoundingClientRect();
     let refPostionStart = refObjRect.top - bodyRect.top;
@@ -79,8 +99,8 @@ function animateOnScroll(obj, refObj, animationIn, animationOut, duration, offse
 
     //Animate in or out
     if (currentPositionBottom > refPostionStart && currentPositionTop < refPostionStart) {
-        if (obj.animatedIn){
-            if(obj.animatedOut && !obj.setTimer){
+        if (obj.animatedIn) {
+            if (obj.animatedOut && !obj.setTimer) {
                 obj.animateInOnEnd = true;
             }
             return;
@@ -100,23 +120,88 @@ function animateOnScroll(obj, refObj, animationIn, animationOut, duration, offse
             obj.animatedOut = false;
             obj.animatedIn = false;
             obj.style.visibility = 'hidden';
-            if(obj.animateInOnEnd){
+            if (obj.animateInOnEnd) {
                 animateOnScroll(obj, refObj, animationIn, animationOut, duration, offsetIn, offsetOut);
             }
         }, { once: true });
     }
 }
 
-function animateAll(domObj){
-    animateOnScroll(domObj.profile, domObj.s1obj, 'fadeInLeft', 'fadeOutLeft',1.5, 100, 50);
-    animateOnScroll(domObj.card, domObj.s1obj, 'fadeInRight', 'fadeOutRight',1.5, 100, 50);
-    animateOnScroll(domObj.an1obj, domObj.s1obj, 'fadeInLeftBig', 'fadeOutLeft',1, 100, 50);
+/**
+ * 
+ * @param {object} domObj objects to animate
+ */
+function animateAll(domObj) {
+    animateOnScroll(domObj.profile, domObj.s1obj, 'fadeInLeft', 'fadeOutLeft', 1.5, 100, 50);
+    animateOnScroll(domObj.card, domObj.s1obj, 'fadeInRight', 'fadeOutRight', 1.5, 100, 50);
+    animateOnScroll(domObj.an1obj, domObj.s1obj, 'fadeInLeftBig', 'fadeOutLeft', 1, 100, 50);
     animateOnScroll(domObj.an2obj, domObj.s2obj, 'fadeInLeftBig', 'fadeOutLeft', 1, 100, 50);
-    animateOnScroll(domObj.an3obj, domObj.s3obj, 'fadeInLeftBig', 'fadeOutLeft',1, 100, 50);
+    animateOnScroll(domObj.an3obj, domObj.s3obj, 'fadeInLeftBig', 'fadeOutLeft', 1, 100, 50);
 }
 
-//Events
-window.addEventListener('load', (e) => {
+/**
+ * 
+ * @param {string} url url of json data
+ */
+function loadData(url) {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            dataLoadCompleted(data);
+        })
+        /*
+        .catch((e) => {
+            alert('Error loading data.');
+        })*/;
+}
+
+/**
+ * 
+ * @param {object} data json data
+ */
+function dataLoadCompleted(data) {
+
+    //Personal information
+    did('data-name').innerText = did('data-title').innerText = data.name.last + ", " + data.name.first;
+    did('data-ocupation').innerText = data.ocupation;
+    did('data-description').innerHTML = '<p>'+replaceAll(data.description,'\n','</p><p>')+'</p>';
+    did('data-picture').src = data.picture;
+    did('data-personal').innerHTML =
+
+
+        'Teléfono: <a class="link-light" href="tel:' + data.cell + '">' + data.cell + '</a>'
+        + '<br>' +
+        'E-mail: <a class="link-light" href="mailto:' + data.email + '">' + data.email + '</a>';
+    + '<br>' +
+        data.location.city + ', ' + data.location.state + ', ' + data.location.country;
+
+
+    //Experience
+
+    //Get template
+    const template = did('data-experience-template').cloneNode(true);
+    template.removeAttribute('id');
+
+    //Get container object
+    const dataexperience = did('data-experience');
+    let newexp;
+
+    //Clear container object
+    let child = dataexperience.lastElementChild;
+    while (child) {
+        dataexperience.removeChild(child);
+        child = dataexperience.lastElementChild;
+    }
+
+    //Add data
+    data.experience.forEach(element => {
+        newexp = template.cloneNode(true);
+        dataexperience.appendChild(newexp);
+    });
+
+
+    //Abilities
+
 
     // Objects to animate and references
     let domObj = {
@@ -139,4 +224,23 @@ window.addEventListener('load', (e) => {
 
     //Call animation on load
     animateAll(domObj);
-})
+}
+
+//Events
+window.addEventListener('load', (e) => {
+
+    //Load data
+    loadData('data/mydata.json');
+
+    //Close nav on clic
+    const navLinks = document.querySelectorAll('.nav-item');
+    const menuToggle = document.getElementById('navbarCollapse');
+    const bsCollapse = new bootstrap.Collapse(menuToggle, { "toggle": false });
+    navLinks.forEach((l) => {
+        l.addEventListener('click', () => {
+            if (window.innerWidth < 768)
+                bsCollapse.toggle();
+        })
+    });
+
+});
